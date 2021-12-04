c98fb2cff26a2c1298f48db5fc9fb3a957008d16
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-04 04:36:47 +0100

    Add CI

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   outputs = _: {
     templates.sbt-nix = {
       path = ./templates/sbt-nix;
-      description = "An sbt project built in a Nix flake";
+      description = ''
+        An sbt project built in a Nix flake.
+        This template is inspired by https://github.com/gvolpe/sbt-nix.g8
+        and uses https://github.com/zaninime/sbt-derivation just like the original, but it's a Flake template.'';
     };
   };
 }

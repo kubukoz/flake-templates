0dc20fb5fe34cb0ed9c2d7cf7bfc705816716caa
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-05 02:14:32 +0100

    Revert "Move template testing to flake checks"
    This reverts commit e50fa7ff9db4b7da3dbebd694002c190e64f7043.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils, ... }: {
+  outputs = _: {
     templates.sbt-nix = {
       path = ./templates/sbt-nix;
       description = ''
@@ -10,14 +7,5 @@
         This template is inspired by https://github.com/gvolpe/sbt-nix.g8
         and uses https://github.com/zaninime/sbt-derivation just like the original, but it's a Flake template.'';
     };
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      lib = import ./lib.nix { inherit pkgs; };
-    in
-    {
-      checks = {
-        sbt-nix-template-check = lib.check-template { templateName = "sbt-nix"; inherit self; };
-      };
-    });
+  };
 }
